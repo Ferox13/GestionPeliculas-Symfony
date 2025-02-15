c785f0be66ef5ab5ae5e1a0b03419a235d6d4488
Fer <feroxtv@gmail.com>
2025-02-15 12:56:04 +0100

    Detalle de pelicula creado

---
+++ src/Controller/DetallePeliculaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class DetallePeliculaController extends AbstractController
+{
+    #[Route('/detalle/pelicula', name: 'app_detalle_pelicula')]
+    public function index(): Response
+    {
+        return $this->render('detalle_pelicula/index.html.twig', [
+            'controller_name' => 'DetallePeliculaController',
+        ]);
+    }
+}
