06b807484cad372268c24f3f13d1c27c2bea31c3
Fer <feroxtv@gmail.com>
2025-02-16 17:27:57 +0100

    creado botón para limpair filtros

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,11 +20,12 @@ final class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(Request $request): Response
     {
-        $titulo = $request->query->get('titulo');
-        $director = $request->query->get('director');
-        $genero = $request->query->get('genero');
-        $duracion = $request->query->get('duracion');
-        $valoracion = $request->query->get('valoracion');
+        $titulo         = $request->query->get('titulo');
+        $director       = $request->query->get('director');
+        $genero         = $request->query->get('genero');
+        $duracion       = $request->query->get('duracion');
+        $valoracion     = $request->query->get('valoracion');
+        $estreno        = $request->query->get('estreno');
 
         $query = $this->peliculaRepository->createQueryBuilder('p');
 
@@ -36,10 +37,23 @@ final class HomeController extends AbstractController
             'valoracion'=> $valoracion,
         ];
 
+        // Filtros de tipo cadena o numéricos
         foreach ($filtros as $key => $value) {
             if (!empty($value)) {
                 $query->andWhere("p.$key LIKE :$key")
                       ->setParameter($key, '%' . $value . '%');
+            }
+        }
+        error_log("Valor de estreno: " . $estreno);
+        dump($estreno);
+
+        // Asegúrate de usar el formato "Y-m-d" para crear el objeto DateTime ya que el input del calendario lo envía en ese formato.
+        if (!empty($estreno)) {
+            error_log("Valor de estreno: " . $estreno);
+            $estrenoDate = \DateTime::createFromFormat('Y-m-d', $estreno);
+            if ($estrenoDate) {
+                $query->andWhere("p.estreno = :estreno")
+                      ->setParameter('estreno', $estrenoDate);
             }
         }
 
@@ -52,7 +66,7 @@ final class HomeController extends AbstractController
 
         return $this->render('home/index.html.twig', [
             'peliculas' => $peliculas,
-            'mensaje' => $mensaje,
+            'mensaje'   => $mensaje,
         ]);
     }
 
