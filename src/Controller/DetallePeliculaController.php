c924a70749235e7124da8b37428f7ff2443e7c53
Fer <feroxtv@gmail.com>
2025-02-16 16:26:48 +0100

    correción detalle pelicula

--- src/Controller/DetallePeliculaController.php
+++ src/Controller/DetallePeliculaController.php
@@ -2,16 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\PeliculaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class DetallePeliculaController extends AbstractController
 {
-    #[Route('/detalle/pelicula', name: 'app_detalle_pelicula')]
-    public function index(): Response
+    #[Route('/detalle/pelicula/{id}', name: 'detalle_pelicula')]
+    public function index(int $id, PeliculaRepository $peliculaRepository): Response
     {
+        $pelicula = $peliculaRepository->find($id);
+        if (!$pelicula) {
+            throw $this->createNotFoundException('La película no existe.');
+        }
+
         return $this->render('detalle_pelicula/index.html.twig', [
+            'pelicula' => $pelicula,
         ]);
     }
 }
