a45b1d8a33520871bd928d5afd215020458948a5
Fer <feroxtv@gmail.com>
2025-02-18 00:25:16 +0100

    Endpoints securizados Crear, editar y eliminar solo permitidos a usuarios logueados

--- src/Controller/EditarPeliculaController.php
+++ src/Controller/EditarPeliculaController.php
@@ -9,10 +9,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class EditarPeliculaController extends AbstractController
 {
     #[Route('/editar/pelicula/{id}', name: 'editar_pelicula')]
+    #[IsGranted('ROLE_USER')]
+
     public function editarPelicula(
         Request $request,
         int $id,
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
@@ -12,10 +15,14 @@ class LoginController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
+        // Si el usuario ya está autenticado, redirige a la ruta "app_home"
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
 
-        // last username entered by the user
+        // Obtener el error de autenticación, si lo hay
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // Obtener el último nombre de usuario ingresado
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('login/login.html.twig', [
@@ -23,9 +30,24 @@ class LoginController extends AbstractController
             'error' => $error,
         ]);
     }
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
+
+    /*TODO Intentar que el logout redirija al home usando esta función */
+    /*   #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): Response
     {
+        return $this->redirectToRoute('app_home');
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    } */
+   
+    #[Route(path: '/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(Request $request, TokenStorageInterface $tokenStorage, SessionInterface $session): Response
+    {
+        // Elimina el token de autenticación
+        $tokenStorage->setToken(null);
+        // Invalida la sesión
+        $session->invalidate();
+
+        // Redirige al home
+        return $this->redirectToRoute('app_home');
     }
 }
