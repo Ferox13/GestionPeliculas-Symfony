6930a0e51170df6802364e11060ba8fbe67444d2
Fer <feroxtv@gmail.com>
2025-02-17 23:41:33 +0100

    Creado securización

---
+++ src/Controller/ApiLoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class ApiLoginController extends AbstractController
+{
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+
+    public function index(#[CurrentUser] ?User $user): Response
+    {
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $token = bin2hex(random_bytes(60)); // generate a random token for $user
+
+        return $this->json([
+            'path' => 'src/Controller/ApiLoginController.php',
+            'user'  => $user->getUserIdentifier(),
+            'token' => $token,
+        ]);
+    }
+}
